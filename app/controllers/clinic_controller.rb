45225582a294384a4ca800e59c056f77faa8eb20
Kenneth <vine@vine.(none)>
2014-06-20 11:03:28 +0200

    removed prescribe button from reports tab

--- app/controllers/clinic_controller.rb
+++ app/controllers/clinic_controller.rb
@@ -19,7 +19,6 @@ class ClinicController < GenericClinicController
   def reports
     @reports = [#['/reports/select/','Booking Cohort Report'],
       ['/reports/report_limits', 'Monthly Report'] ,
-      ['/prescriptions/psb_challenge', 'Prescribe'] ,
       ['/reports/select?type=anc_cohort', 'Booking Cohort Report']]
 
     # render :template => 'clinic/reports', :layout => 'clinic'
