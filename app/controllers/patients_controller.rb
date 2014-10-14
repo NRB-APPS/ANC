1d347897e2d09fc7b0d1967038e07477d6b35186
Kenneth <kennethkapundi1@gmail.com>
2014-10-14 14:29:13 +0200

    fixed a bug on Current Pregnancy after non-anc predefined drugs

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -861,9 +861,9 @@ class PatientsController < ApplicationController
         drug_name = o.drug_order.drug.name.match(/syrup|\d+\.*\d+mg|\d+\.*\d+\smg|\d+\.*\d+ml|\d+\.*\d+\sml/i) ?
           (o.drug_order.drug.name[0, o.drug_order.drug.name.index(" ")] + " " +
             o.drug_order.drug.name.match(/syrup|\d+\.*\d+mg|\d+\.*\d+\smg|\d+\.*\d+ml|\d+\.*\d+\sml/i)[0]) :
-          (o.drug_order.drug.name[0, o.drug_order.drug.name.index(" ")])
+          (o.drug_order.drug.name[0, o.drug_order.drug.name.index(" ")]) rescue o.drug_order.drug.name
 
-        if main_drugs.include?(o.drug_order.drug.name[0, o.drug_order.drug.name.index(" ")])
+        if (main_drugs.include?(o.drug_order.drug.name[0, o.drug_order.drug.name.index(" ")]) rescue false)
 
           @drugs[e.encounter_datetime.strftime("%d/%b/%Y")][o.drug_order.drug.name[0,
               o.drug_order.drug.name.index(" ")]] = o.drug_order.amount_needed
