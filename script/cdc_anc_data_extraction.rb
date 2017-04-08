6cbaa49659354138135b2e2eb75e5425cce56c35
Deliwe Nkhoma <dbnkhoma@gmail.com>
2017-04-08 17:47:17 +0200

    Modified CDC ANC data extraction to be saved as csv

--- script/cdc_anc_data_extraction.rb
+++ script/cdc_anc_data_extraction.rb
@@ -9,28 +9,13 @@ def start
   end_date = "2016-12-31".to_date
   puts "CDC ANC data extraction............................................................................................"
   puts "HIV status Known results (Newly).............................................................................."
-  newly_registered_pmtct_known_results = newly_registered_pmtct_known_results(start_date, end_date)
-  newly_registered_pmtct_known_results_less_than_15 = newly_registered_pmtct_known_results(start_date, end_date, 0, 14)
-  newly_registered_pmtct_known_results_between_15_19 = newly_registered_pmtct_known_results(start_date, end_date, 15, 19)
-  newly_registered_pmtct_known_results_between_20_24 = newly_registered_pmtct_known_results(start_date, end_date, 20, 24)
-  newly_registered_pmtct_known_results_between_25_49 = newly_registered_pmtct_known_results(start_date, end_date, 25, 49)
-  newly_registered_pmtct_known_results_more_than_50 = newly_registered_pmtct_known_results(start_date, end_date, 50, nil)
+  newly_registered_pmtct_known_results, newly_registered_pmtct_known_results_less_than_15, newly_registered_pmtct_known_results_between_15_19, newly_registered_pmtct_known_results_between_20_24, newly_registered_pmtct_known_results_between_25_49, newly_registered_pmtct_known_results_more_than_50 = newly_registered_pmtct_known_results(start_date, end_datel)
 
   puts "HIV status Known results (Cumulative)........................................................................."
-  total_pmtct_known_results = pmtct_known_results(start_date, end_date)
-  pmtct_known_results_less_than_15 = pmtct_known_results(start_date, end_date, 0, 14)
-  pmtct_known_results_between_15_19 = pmtct_known_results(start_date, end_date, 15, 19)
-  pmtct_known_results_between_20_24 = pmtct_known_results(start_date, end_date, 20, 24)
-  pmtct_known_results_between_25_49 = pmtct_known_results(start_date, end_date, 25, 49)
-  pmtct_known_results_more_than_50 = pmtct_known_results(start_date, end_date, 50, nil)
+  total_pmtct_known_results, pmtct_known_results_less_than_15, pmtct_known_results_between_15_19, pmtct_known_results_between_20_24, pmtct_known_results_between_25_49, pmtct_known_results_more_than_50 = pmtct_known_results(start_date, end_date)
 
   puts "HIV status Known at entry results.............................................................................."
-  total_hiv_status_known_at_entry_positive = hiv_status_known_at_entry_positive(start_date, end_date)
-  hiv_status_known_at_entry_positive_less_than_15 = hiv_status_known_at_entry_positive(start_date, end_date, 0, 14)
-  hiv_status_known_at_entry_positive_between_15_19 = hiv_status_known_at_entry_positive(start_date, end_date, 15, 19)
-  hiv_status_known_at_entry_positive_between_20_24 = hiv_status_known_at_entry_positive(start_date, end_date, 20, 24)
-  hiv_status_known_at_entry_positive_between_25_49 = hiv_status_known_at_entry_positive(start_date, end_date, 25, 49)
-  hiv_status_known_at_entry_positive_more_than_50 = hiv_status_known_at_entry_positive(start_date, end_date, 50, nil)
+  total_hiv_status_known_at_entry_positive, hiv_status_known_at_entry_positive_less_than_15, hiv_status_known_at_entry_positive_between_15_19, hiv_status_known_at_entry_positive_between_20_24, hiv_status_known_at_entry_positive_between_25_49, hiv_status_known_at_entry_positive_more_than_50 = hiv_status_known_at_entry_positive(start_date, end_date, 50, nil)
 
   puts "PMTCT life long ART results...................................................................................."
   total_pmtct_life_long_art_results = pmtct_life_long_art_cumulative(start_date, end_date)
@@ -38,31 +23,29 @@ def start
   pmtct_life_long_art_already = pmtct_life_long_art_cumulative(start_date, end_date)
 
   if CDCDataExtraction == 1
-    $resultsOutput = File.open("./CDCDataExtraction_ANC" + "#{facility_name}" + ".txt", "w")
-    $resultsOutput  << "HIV status Known results (Newly registered)..........................................................\n"
-    $resultsOutput  << " newly_registered_pmtct_known_results: #{newly_registered_pmtct_known_results}\n newly_registered_pmtct_known_results_less_than_15: #{newly_registered_pmtct_known_results_less_than_15}\n newly_registered_pmtct_known_results_between_15_19: #{newly_registered_pmtct_known_results_between_15_19}\n newly_registered_pmtct_known_results_between_20_24: #{newly_registered_pmtct_known_results_between_20_24}\n newly_registered_pmtct_known_results_between_25_49: #{newly_registered_pmtct_known_results_between_25_49}\n newly_registered_pmtct_known_results_more_than_50: #{newly_registered_pmtct_known_results_more_than_50}\n\n"
-    $resultsOutput  << "HIV status Known results (Cumulative)..........................................................\n"
-    $resultsOutput  << " total_pmtct_known_results: #{total_pmtct_known_results}\n pmtct_known_results_less_than_15: #{pmtct_known_results_less_than_15}\n pmtct_known_results_between_15_19: #{pmtct_known_results_between_15_19}\n pmtct_known_results_between_20_24: #{pmtct_known_results_between_20_24}\n pmtct_known_results_between_25_49: #{pmtct_known_results_between_25_49}\n pmtct_known_results_more_than_50: #{pmtct_known_results_more_than_50}\n\n"
-    $resultsOutput  << "HIV status known at entry results..........................................................\n"
-    $resultsOutput  << " total_hiv_status_known_at_entry_positive: #{total_hiv_status_known_at_entry_positive}\n hiv_status_known_at_entry_positive_less_than_15: #{hiv_status_known_at_entry_positive_less_than_15}\n hiv_status_known_at_entry_positive_between_15_19: #{hiv_status_known_at_entry_positive_between_15_19}\n hiv_status_known_at_entry_positive_between_20_24: #{hiv_status_known_at_entry_positive_between_20_24}\n hiv_status_known_at_entry_positive_between_25_49: #{hiv_status_known_at_entry_positive_between_25_49}\n hiv_status_known_at_entry_positive_more_than_50: #{hiv_status_known_at_entry_positive_more_than_50}\n\n"
-    $resultsOutput  << "PMTCT life long ART results..........................................................\n"
-    $resultsOutput  << " total_pmtct_life_long_art_results: #{total_pmtct_life_long_art_results}\n pmtct_life_long_art_newly: #{pmtct_life_long_art_newly}\n pmtct_life_long_art_already: #{pmtct_life_long_art_already}\n"
+    file = "/home/deliwe/Desktop/cdc_data_extraction/cdc_anc_date_extraction_" + "#{facility_name}" + ".csv"
+
+    FasterCSV.open( file, 'w' ) do |csv|
+      csv << ["Facility_Name", "Category", "Total of Category", "Less_than_15yrs", "Between_15_and_19_yrs", "Between_20_24_yrs", "Between_25_49_yrs", "More_than_50yrs"]
+
+      csv << ["#{facility_name}", "HIV status Known results (Newly)", "#{newly_registered_pmtct_known_results}", "#{newly_registered_pmtct_known_results_less_than_15}", "#{newly_registered_pmtct_known_results_between_15_19}", "#{newly_registered_pmtct_known_results_between_20_24}", "#{newly_registered_pmtct_known_results_between_25_49}", "#{newly_registered_pmtct_known_results_more_than_50}"]
+
+	    csv << ["#{facility_name}", "HIV status Known results (Cumulative)", "#{total_pmtct_known_results}", "#{pmtct_known_results_less_than_15}", "#{pmtct_known_results_between_15_19}", "#{pmtct_known_results_between_20_24}", "#{pmtct_known_results_between_25_49}", "#{pmtct_known_results_more_than_50}"]
+
+      csv << ["#{facility_name}", "HIV status Known at entry results","#{total_hiv_status_known_at_entry_positive}", "#{hiv_status_known_at_entry_positive_less_than_15}", "#{hiv_status_known_at_entry_positive_between_15_19}", "#{hiv_status_known_at_entry_positive_between_20_24}", "#{hiv_status_known_at_entry_positive_between_25_49}", "#{hiv_status_known_at_entry_positive_more_than_50}"]
+
+      csv << ["Facility_Name", "", "Total_pmtct_life_long_art_results", "PMTCT_life_long_art_newly", "PMTCT_life_long_art_already"]
+      csv << ["#{facility_name}", "PMTCT life long ART results", "total_pmtct_life_long_art_results", "pmtct_life_long_art_newly", "pmtct_life_long_art_already"]
+    end
   end
 
   if CDCDataExtraction == 1
-    $resultsOutput.close()
+    #{}$resultsOutput.close()
   end
+
 end
 
 def self.pmtct_known_results(start_date, end_date, min_age = nil, max_age = nil)
-  if (max_age.blank? && min_age.blank?)
-    condition = ""
-  elsif (max_age.blank?)
-    condition = "HAVING age  >= #{min_age}"
-  else
-    condition = "HAVING age BETWEEN #{min_age} and #{max_age}"
-  end
-
   #HIV status = 3753
   #positive = 703
   pmtct_with_known_results = ActiveRecord::Base.connection.select_all <<EOF
@@ -77,27 +60,40 @@ def self.pmtct_known_results(start_date, end_date, min_age = nil, max_age = nil)
                                 AND obs.person_id = o.person_id
                                 AND DATE(obs.obs_datetime) <= '#{end_date}'
     							AND (obs.value_text = 'Positive' OR obs.value_coded = 703))
-   GROUP BY o.person_id
-    #{condition};
+   GROUP BY o.person_id;
 EOF
 
-  if pmtct_with_known_results.blank?
-    result = 0
-  else
-    result = pmtct_with_known_results.count
+  total_pmtct_known_results = []
+  pmtct_known_results_less_than_15 = []
+  pmtct_known_results_between_15_19 = []
+  pmtct_known_results_between_20_24 = []
+  pmtct_known_results_between_25_49 = []
+  pmtct_known_results_more_than_50 = []
+
+  (pmtct_with_known_results || []),each do |patient|
+    if (patient['age'].to_i  <= 14)
+      pmtct_known_results_less_than_15 << patient['person_id'].to_i
+    elsif (patient['age'].to_i >= 15 and patient['age'].to_i <= 19)
+      pmtct_known_results_between_15_19 << patient['person_id'].to_i
+    elsif (patient['age'].to_i >= 20 and patient['age'].to_i <= 24)
+      pmtct_known_results_between_20_24 << patient['person_id'].to_i
+    elsif (patient['age'].to_i >= 25 and patient['age'].to_i <= 49)
+      pmtct_known_results_between_25_49 << patient['person_id'].to_i
+    elsif (patient['age'].to_i  >= 50)
+      pmtct_known_results_more_than_50 << patient['person_id'].to_i
+    end
+    total_pmtct_known_results << patient['person_id'].to_i
   end
-  return  result
+
+  return [total_pmtct_known_results.count,
+      pmtct_known_results_less_than_15.count,
+      pmtct_known_results_between_15_19.count,
+      pmtct_known_results_between_20_24.count,
+      pmtct_known_results_between_25_49.count,
+      pmtct_known_results_more_than_50.count]
 end
 
 def self.newly_registered_pmtct_known_results(start_date, end_date, min_age = nil, max_age = nil)
-  if (max_age.blank? && min_age.blank?)
-    condition = ""
-  elsif (max_age.blank?)
-    condition = "HAVING age  >= #{min_age}"
-  else
-    condition = "HAVING age BETWEEN #{min_age} and #{max_age}"
-  end
-
   #HIV status = 3753
   #positive = 703
   pmtct_with_known_results = ActiveRecord::Base.connection.select_all <<EOF
@@ -112,27 +108,39 @@ def self.newly_registered_pmtct_known_results(start_date, end_date, min_age = ni
                                 AND obs.person_id = o.person_id
                                 AND DATE(obs.obs_datetime) BETWEEN '#{start_date}' AND '#{end_date}'
     							AND (obs.value_text = 'Positive' OR obs.value_coded = 703))
-    GROUP BY o.person_id
-    #{condition};
+    GROUP BY o.person_id;
 EOF
+  newly_registered_pmtct_known_results = []
+  newly_registered_pmtct_known_results_less_than_15 = []
+  newly_registered_pmtct_known_results_between_15_19 = []
+  newly_registered_pmtct_known_results_between_20_24 = []
+  newly_registered_pmtct_known_results_between_25_49 = []
+  newly_registered_pmtct_known_results_more_than_50 = []
 
-  if pmtct_with_known_results.blank?
-    result = 0
-  else
-    result = pmtct_with_known_results.count
+  (pmtct_with_known_results || []),each do |patient|
+    if (patient['age'].to_i  <= 14)
+      newly_registered_pmtct_known_results_less_than_15 << patient['person_id'].to_i
+    elsif (patient['age'].to_i >= 15 and patient['age'].to_i <= 19)
+      newly_registered_pmtct_known_results_between_15_19 << patient['person_id'].to_i
+    elsif (patient['age'].to_i >= 20 and patient['age'].to_i <= 24)
+      newly_registered_pmtct_known_results_between_20_24 << patient['person_id'].to_i
+    elsif (patient['age'].to_i >= 25 and patient['age'].to_i <= 49)
+      newly_registered_pmtct_known_results_between_25_49 << patient['person_id'].to_i
+    elsif (patient['age'].to_i  >= 50)
+      newly_registered_pmtct_known_results_more_than_50 << patient['person_id'].to_i
+    end
+    newly_registered_pmtct_known_results << patient['person_id'].to_i
   end
-  return  result
+
+  return [newly_registered_pmtct_known_results.count,
+      newly_registered_pmtct_known_results_less_than_15.count,
+      newly_registered_pmtct_known_results_between_15_19.count,
+      newly_registered_pmtct_known_results_between_20_24.count,
+      newly_registered_pmtct_known_results_between_25_49.count,
+      newly_registered_pmtct_known_results_more_than_50.count]
 end
 
 def self.hiv_status_known_at_entry_positive(start_date, end_date, min_age = nil, max_age = nil)
-  if (max_age.blank? && min_age.blank?)
-    condition = ""
-  elsif (max_age.blank?)
-    condition = " AND age  >= #{min_age}"
-  else
-    condition = "AND (age BETWEEN #{min_age} and #{max_age})"
-  end
-
   hiv_status_concept_id = 3753 #(SELECT concept_id FROM concept_name WHERE name = 'HIV status' LIMIT 1)
   positive_concept_id = 703 #(SELECT concept_id FROM concept_name WHERE name = 'Positive' LIMIT 1)
   hiv_test_date_concept_id = 1837 #(SELECT concept_id FROM concept_name WHERE name = 'HIV test date' LIMIT 1)
@@ -156,15 +164,37 @@ def self.hiv_status_known_at_entry_positive(start_date, end_date, min_age = nil,
                           AND DATE(encounter.encounter_datetime) <= '#{end_date}')
     AND (DATE(e.encounter_datetime) <= '#{end_date}')
     GROUP BY e.patient_id
-    HAVING DATE(date) = DATE(test_date) #{condition};
+    HAVING DATE(date) = DATE(test_date);
 EOF
 
-  if hiv_status_known_at_entry_positive_results.blank?
-    result = 0
-  else
-    result = hiv_status_known_at_entry_positive_results.count
+  total_hiv_status_known_at_entry_positive = []
+  hiv_status_known_at_entry_positive_less_than_15 = []
+  hiv_status_known_at_entry_positive_between_15_19 = []
+  hiv_status_known_at_entry_positive_between_20_24 = []
+  hiv_status_known_at_entry_positive_between_25_49 = []
+  hiv_status_known_at_entry_positive_more_than_50 = []
+
+  (hiv_status_known_at_entry_positive_results || []),each do |patient|
+    if (patient['age'].to_i  <= 14)
+      hiv_status_known_at_entry_positive_less_than_15 << patient['patient_id'].to_i
+    elsif (patient['age'].to_i >= 15 and patient['age'].to_i <= 19)
+      hiv_status_known_at_entry_positive_between_15_19 << patient['patient_id'].to_i
+    elsif (patient['age'].to_i >= 20 and patient['age'].to_i <= 24)
+      hiv_status_known_at_entry_positive_between_20_24 << patient['patient_id'].to_i
+    elsif (patient['age'].to_i >= 25 and patient['age'].to_i <= 49)
+      hiv_status_known_at_entry_positive_between_25_49 << patient['patient_id'].to_i
+    elsif (patient['age'].to_i  >= 50)
+      hiv_status_known_at_entry_positive_more_than_50 << patient['patient_id'].to_i
+    end
+    total_hiv_status_known_at_entry_positive << patient['patient_id'].to_i
   end
-  return  result
+
+  return [total_hiv_status_known_at_entry_positive.count,
+      hiv_status_known_at_entry_positive_less_than_15.count,
+      hiv_status_known_at_entry_positive_between_15_19.count,
+      hiv_status_known_at_entry_positive_between_20_24.count,
+      hiv_status_known_at_entry_positive_between_25_49.count,
+      hiv_status_known_at_entry_positive_more_than_50.count]
 end
 
 def self.pmtct_life_long_art_newly(start_date, end_date, min_age = nil, max_age = nil)
